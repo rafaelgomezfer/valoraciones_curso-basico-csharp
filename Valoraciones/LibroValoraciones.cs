381c4321ac7434f66a9f2a0c5ced54c0b076dfd9
rafagomezfer <rafagf89@gmail.com>
2018-01-09 12:02:43 -0500

    añade calcular valoraciones: promedio, valor mínimo y máximo

--- Valoraciones/LibroValoraciones.cs
+++ Valoraciones/LibroValoraciones.cs
@@ -14,11 +14,28 @@ namespace Valoraciones
         {
             valoraciones = new List<float>();
         }
+
+        public CalcularValoraciones PublicarValoraciones()
+        {
+            CalcularValoraciones calculo = new CalcularValoraciones();
+            float sumaValoraciones = 0;
+            foreach (float valoracion in valoraciones)
+            {
+
+                calculo.valoracionMin = Math.Min(valoracion,calculo.valoracionMin);
+                calculo.valoracionMax = Math.Max(valoracion, calculo.valoracionMax);
+                //sumaValoraciones = sumaValoraciones + valoracion;
+                sumaValoraciones += valoracion;
+            }
+
+            calculo.promedioValoraciones = sumaValoraciones / valoraciones.Count;
+            return calculo;
+        }
+
         public void AgregarValoraciones(float valoracion)
         {
             valoraciones.Add(valoracion);
         }
-        
-
+     
     }
 }
--- Valoraciones/Program.cs
+++ Valoraciones/Program.cs
@@ -10,9 +10,25 @@ namespace Valoraciones
     {
         static void Main(string[] args)
         {
+            //Crear objeto libro
             LibroValoraciones libro =new LibroValoraciones();
-            libro.AgregarValoraciones(3.5f);
-            libro.AgregarValoraciones(5);
+
+            //Asignar Valoraciones
+            libro.AgregarValoraciones(5); //si es numero con decimales .> 3.5f
+            libro.AgregarValoraciones(4);
+            libro.AgregarValoraciones(3);
+            libro.AgregarValoraciones(2);
+            libro.AgregarValoraciones(1);
+     
+            // Publicar Valoraciones
+            CalcularValoraciones publicar = libro.PublicarValoraciones();
+            Console.WriteLine("El promedio de las valoraciones es : " + publicar.promedioValoraciones);
+            Console.WriteLine("La valoración máxima es : " + publicar.valoracionMax);
+            Console.WriteLine("La valoración mínima es : " + publicar.valoracionMin);
+            Console.ReadLine();
+            // valoracionMinima
+            // ValoracionMaxima
+            // promedioValoraciones
         }
     }
 }
